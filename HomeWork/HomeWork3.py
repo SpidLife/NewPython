ab0fbba372765c7de43933c4a055c66eba7285af
SpidLife <dahm01@mail.ru>
2022-12-05 21:28:20 +0500

    ДЗ3

---
+++ HomeWork/HomeWork3.py
@@ -0,0 +1,104 @@
+# Задание №1
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+# Пример:
+
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+# def sum_odd_index(lst):
+#     s = 0
+#     for i in range(len(lst)):
+#         if i % 2 != 0:
+#             s += lst[i]
+#     print(f"Сумма равна: {s}")
+
+# lst = [2, 3, 5, 9, 3]
+# sum_odd_index(lst)
+# lst = list(map(int, input("Введите числа через пробел:\n").split()))
+# sum_odd_index(lst)
+
+# Задание №2
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+# from random import randint
+
+# number = int(input('Введите размер списка '))
+# list = []
+# list2 = []
+
+# for i in range(number):
+#     list.append(randint(0, 9))
+
+# for i in range(len(list)):
+#     while i < len(list)/2 and number > len(list)/2:
+#         number = number - 1
+#         a = list[i] * list[number]
+#         list2.append(a)
+#         i += 1
+
+# print(list)
+# print(list2)
+
+# Задание №3
+
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+# Пример:
+
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+# lst = list(map(float, input("Введите числа через пробел:\n").split()))
+# new_lst = [round(i%1,2) for i in lst if i%1 != 0]
+# print(max(new_lst) - min(new_lst))
+
+## Задание 4
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+# Пример:
+
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+# s = ""
+# n = int(input("Введите число для преобразовывания десятичного числа в двоичное:\n"))
+# while n != 0:
+#     s = str(n%2) + s
+#     n //=2
+# print(s)
+
+## Задание 5
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+# Пример:
+
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+# def Fibonacci(n):
+#     if n in [1, 2]:                       
+#         return 1
+#     else:
+#         return Fibonacci(n-1) + Fibonacci(n-2)
+
+# def NegaFibonacci(n):
+#     if n == 1:                       
+#         return 1
+#     elif n == 2:                       
+#         return -1
+#     else:
+#         num1, num2 = 1, -1
+#         for i in range(2, n):
+#             num1, num2 = num2, num1 - num2
+#         return num2
+
+# list = [0]
+# userNumber = int(input('Введите любое число: '))
+# for e in range(1, userNumber + 1):
+#     list.append(Fibonacci(e))
+#     list.insert(0, NegaFibonacci(e))
+# print(list)
